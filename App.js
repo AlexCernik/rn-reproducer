e74357ac88be1234d1e33d9d64c8bba37f7804d9
AlexCernik <cernik@hotmail.es>
2025-12-18 15:58:25 -0300

    commit 2

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ScrollView, Pressable, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 const Stack = createNativeStackNavigator();
 
 const Screen1 = ({ navigation: { navigate } }) => (
@@ -22,13 +23,30 @@ const Screen1 = ({ navigation: { navigate } }) => (
   </ScrollView>
 )
 
-const Screen2 = () => (
+const Screen2 = ({ navigation: { navigate } }) => (
   <ScrollView
     showsVerticalScrollIndicator={false}
     keyboardShouldPersistTaps='handled'
     contentContainerStyle={{ flexGrow: 1, padding: 8, backgroundColor: 'green' }}
   >
     <Text style={{ fontSize: 26, textAlign: 'center', color: 'white' }}>With headerShown false</Text>
+    <Pressable
+      onPress={() => navigate('Screen3')}
+      style={{ marginTop: 20, paddingHorizontal: 16, paddingVertical: 8, borderRadius: 8, backgroundColor: 'red' }}
+    >
+      <Text style={{ color: 'white', textAlign: 'center' }}>Go Screen3</Text>
+    </Pressable>
+    <Text style={{ position: 'absolute', bottom: 20, fontSize: 40, alignSelf: 'center', color: 'white' }}>↓</Text>
+  </ScrollView>
+)
+
+const Screen3 = () => (
+  <ScrollView
+    showsVerticalScrollIndicator={false}
+    keyboardShouldPersistTaps='handled'
+    contentContainerStyle={{ flexGrow: 1, padding: 8, backgroundColor: 'red' }}
+  >
+    <Text style={{ fontSize: 26, textAlign: 'center', color: 'white' }}>With headerShown true</Text>
     <Text style={{ position: 'absolute', bottom: 20, fontSize: 40, alignSelf: 'center', color: 'white' }}>↓</Text>
   </ScrollView>
 )
@@ -52,6 +70,11 @@ const RootStack = () => (
         title: 'Screen2',
         headerShown: false
       }}
+    />
+    <Stack.Screen
+      name='Screen3'
+      component={Screen3}
+      options={{ title: 'Screen3' }}
     />
   </Stack.Navigator>
 )
